9defca7969822386fc65af7b5a5714cfd161a659
albechen <59457095+albechen@users.noreply.github.com>
2020-02-21 11:46:35 -0500

    docs: commenting c_model_fitting.py

--- ACS_analysis_pkg/c_model_fitting.py
+++ ACS_analysis_pkg/c_model_fitting.py
@@ -5,15 +5,20 @@ from sklearn.model_selection import GridSearchCV, cross_val_score, StratifiedKFo
 from .b_preprocessing_pipeline import preprocess_modeling
 from .a_parse_yearly_df import parse_skip_year, parse_single, parse_group
 
-
+# fitting model with simle rfr model
 def rfr_fit(modeling_df):
-    #kfold = StratifiedKFold(n_splits=3)
+    #kfold = StratifiedKFold(n_splits=3) #if smaller dataset
     y_full = modeling_df['WAGP']
     x_full = modeling_df.drop(['WAGP'], axis=1)
+    
+    #splitting for testing set
     x_train_valid, x_test, y_train_valid, y_test = train_test_split(
         x_full, y_full, test_size=0.5, random_state=0)
+    #splitting for validation set
     x_train, x_valid, y_train, y_valid = train_test_split(
         x_train_valid, y_train_valid, test_size=0.02, random_state=0)
+    
+    #checking to see if df is too small so min 1 mil points are used
     if len(x_train.index) <= 1000000:
         x_train = x_train
     else:
@@ -23,6 +28,7 @@ def rfr_fit(modeling_df):
     else:
         y_train = y_train.sample(n=1000000, random_state=1)
 
+    #rfr hyper parameters simple split scoring with MAE
     RFR = RandomForestRegressor(random_state=1, n_jobs=4)
     rfr_param_grid = {'n_estimators': [75, 100, 125],
                       'min_samples_split': [3, 5, 7],
@@ -32,8 +38,10 @@ def rfr_fit(modeling_df):
     gsRFR.fit(x_valid, y_valid)
     #print(gsRFR.best_score_)
     RFR_best = gsRFR.best_estimator_
-
+    
     RFR_best.fit(x_train, y_train)
+    
+    #Used prior for testing model orignally when tunning hyper parameters
     #test_prediction = RFR_best.predict(x_test)
     #print(mean_absolute_error(y_test, test_prediction))
 
