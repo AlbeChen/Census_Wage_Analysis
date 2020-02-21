82387dc1b5a0ad14ce14b088e427e986765aa646
albechen <59457095+albechen@users.noreply.github.com>
2020-02-21 11:54:13 -0500

    docs: commenting d_df_transform.py

--- ACS_analysis_pkg/d_df_transform.py
+++ ACS_analysis_pkg/d_df_transform.py
@@ -5,7 +5,11 @@ from .b_preprocessing_pipeline import preprocess_catagories, preprocess_modeling
 from .a_parse_yearly_df import parse_single
 from .c_model_fitting import rfr_fit
 
+
+#used to create a dummy dataframe that had all permutations of variables
+#to be used to predict all possible combinations of variables
 def create_df_all_variations(cat_df, model_fit):
+    #create empty dataframe with all combinations
     grp_list = []
     for sex in list(cat_df.SEX.unique()):
         sex_keep = []
@@ -24,6 +28,8 @@ def create_df_all_variations(cat_df, model_fit):
                         job_keep.append(job)
                         grp_list.append(job_keep)
     grp_list = pd.DataFrame(grp_list, columns =['SEX', 'EDU', 'AGE', 'RACE', 'JOB'])
+    
+    #use OHE_no_WAGP to convert dummy df to OHE and use a prior model fit to predict dummy df
     cat_df_cols = cat_df.drop(['WAGP'], axis=1)
     grp_list = grp_list[cat_df_cols.columns]
     all_var_model = OHE_no_WAGP(grp_list)
@@ -31,16 +37,20 @@ def create_df_all_variations(cat_df, model_fit):
     predicted = pd.DataFrame({'Index': all_var_model.index,
                               'WAG': pred_wages}).set_index('Index')
     predicted = pd.concat([predicted, grp_list], axis=1, sort=False)
-
+    
+    #output of dummy df of all combintations with predicted wages for each depending on df the model fit to
     predicted['WAG'] = np.exp(predicted['WAG'])
     predicted = predicted.sort_values(by=['SEX', 'EDU', 'JOB', 'RACE', 'AGE'])
     predicted = predicted.reset_index(drop=True)
     return predicted
 
 
+#agg data by wage to grab orignal df's population count of each combiation of person
 def group_by_category(cat_df):
     grouped = cat_df.groupby(['SEX', 'EDU', 'JOB', 'RACE', 'AGE']) \
         .agg({'WAGP': ['count', 'mean']})
+    
+    #used to keep all combinations even if they have no one in them
     for level in range(-3,1):
         grouped = grouped.unstack(level=level, fill_value=0).stack()
     grouped = grouped.unstack(level=-4, fill_value=0).stack().reset_index()
@@ -56,6 +66,9 @@ def group_by_category(cat_df):
     return grouped
 
 
+#full summary of above functions ^^^^
+#var_df is the dummy df with all possible combinations
+#cat_grp is the raw df with mean and count of all combinations
 def transform_for_scoring(raw_df):
     cat_df = preprocess_catagories(raw_df)
     mod_df = preprocess_modeling(raw_df)
